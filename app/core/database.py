92aa4f36d96a7b35e404e762211c46e9b9dce2e0
Solohunt307 <kovendhan.s3@gmail.com>
2026-04-10 10:41:53 +0530

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
-# ✅ Use ENV variable (TiDB in production)
-DATABASE_URL = settings.DATABASE_URL
-
 engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "ssl": {
+            "ca": "ca.pem"
+        }
+    }
 )
 
 SessionLocal = sessionmaker(
@@ -17,11 +19,3 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
